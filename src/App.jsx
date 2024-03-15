6b3727bf45cce8112a29975617de111a92a5231d
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2024-03-15 22:32:56 +0530

    initiate

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-zinc-900">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
